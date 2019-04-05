598fd764bac8a845feb99b600cd2d792d6b5d3d1
A_S <suhendaragung20@gmail.com>
2019-04-05 09:41:04 +0700

    Add files via upload

---
+++ local_camera.py
@@ -0,0 +1,13 @@
+import socket
+import cv2
+import random
+import _thread
+
+host = "10.14.6.163"  # e.g. localhost, 192.168.1.123.
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+client_socket.connect((host, 5005))
+
+while True:
+    a = 1
